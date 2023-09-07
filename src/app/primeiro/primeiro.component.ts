4bcda8bd8dec3e1fb6280163050d2d0f0d3a20b2
alexandre-trapp <alexandre.trapp@hotmail.com>
2023-09-07 00:18:04 -0300

    .

---
+++ src/app/primeiro/primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'primeiro-component',
+    template: `
+    <br>
+    <p>Meu primeiro component com Angular2</p>
+    `
+})
+export class PrimeiroComponent { }
